f622364ac5f2e2f920af560d129a34ac32e8cfc1
Krasimir Kostadinov <krasimir.kostadinov.dev@gmail.com>
2021-12-21 13:44:31 +0200

    added mongoDB auth

--- dist/index.js
+++ dist/index.js
@@ -2,9 +2,17 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const express = require("express");
 const dotenv = require("dotenv");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 dotenv.config();
 const PORT = process.env.PORT || 9000;
+const mongoUri = process.env.CONNECTION_URL;
+console.log(mongoUri);
 const app = express();
+app.use(bodyParser.json({
+    limit: "30mb"
+}));
+app.use(cors());
 app.get("/", (req, res) => {
     res.send("home");
 });
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,24 @@
 import * as express from "express";
 import * as dotenv from "dotenv";
+import * as bodyParser from "body-parser";
+import * as mongoose from "mongoose";
+import * as cors from "cors";
 
 dotenv.config();
 
 const PORT = process.env.PORT || 9000;
+const mongoUri = process.env.CONNECTION_URL;
+
+console.log(mongoUri);
+
 const app = express();
+
+app.use(bodyParser.json({
+   limit: "30mb"
+}));
+
+app.use(cors());
+
 
 app.get("/", (req, res) => {
    res.send("home");

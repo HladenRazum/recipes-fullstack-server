2184814ee5ee3ad5bd44f7c2bc121c553ff8cd62
Krasimir Kostadinov <krasimir.kostadinov.dev@gmail.com>
2022-01-10 12:08:13 +0200

    asdf

--- src/models/recipe.model.ts
+++ src/models/recipe.model.ts
@@ -1,9 +1,11 @@
 import * as mongoose from "mongoose";
+import { Ingredient } from "./ingredient.modle";
 
 const recipeSchema = new mongoose.Schema({
    name: { type: String, requred: true },
    category: { type: String, required: true },
    instructions: { type: String, required: true },
+   ingredients: { type: [Ingredient], required: true }
 });
 
 const Recipe = mongoose.model("Recipe", recipeSchema);
